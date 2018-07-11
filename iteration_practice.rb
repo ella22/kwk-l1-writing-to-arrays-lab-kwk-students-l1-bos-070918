1d8bd36527a684bc44e29fe30a81b245aa9d2eab
Ella Sklar <ella.soccer22@gmail.com>
2018-07-11 18:11:18 +0000

    automatically backed up by learn

--- iteration_practice.rb
+++ iteration_practice.rb
@@ -1,9 +1,9 @@
-new_numbers = []
+increase = []
 
 numbers = [1,2,3,4]
 numbers.each do |x|
-  new_numbers = x + 1 
-  new_numbers << new_numbers
+ new_number = x + 1 
+  increase << new_number
 end 
 
-puts new_numbers
+puts increase
